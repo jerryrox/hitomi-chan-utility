c6c281a4f8abb464db6791ebb752427ee36c0e8e
jerryrox <jerryrox@live.co.kr>
2018-06-19 21:12:04 +1200

    Exports addition

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const url = require("./url");
 const util = require("./util");
 
+const PageInfo = require("./classes/PageInfo");
+
 module.exports = {
     url,
-    util
+    util,
+
+    PageInfo
 };
