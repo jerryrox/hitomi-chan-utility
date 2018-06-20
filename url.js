e6ac75de67c366300afe41645fb34647d20fdd61
jerryrox <jerryrox@live.co.kr>
2018-06-20 14:27:35 +1200

    Added streaming url

--- url.js
+++ url.js
@@ -10,6 +10,13 @@ function getBaseUrl(cdn = null) {
 }
 
 /**
+ * Returns the base stream url of hitomi.la
+ */
+function getStreamingUrl() {
+    return "https://streaming.hitomi.la";
+}
+
+/**
  * Returns the url of a json file that contains tag metadata.
  */
 function getTagsUrl() {
@@ -38,6 +45,19 @@ function getPageFileUrl(galleryId, fileName) {
 }
 
 /**
+ * Returns the url of a video file that represents an anime in gallery with specified values.
+ * @param {string} galleryName
+ */
+function getAnimeFileUrl(galleryName) {
+    let fileName = galleryName.toLowerCase()
+        .replace(/ {0,}- {0,}/g, "-") // Hyphens (code 45) should not be surrounded by any space character.
+        .replace(/ {0,}– {0,}/g, "-") // Hyphens (code 8211) should not be surrounded by any space character.
+        .replace(/ +/g, "-") // Replace empty spaces with hyphens.
+        .replace(/[^A-Za-z0-9-]/g, ""); // Remove all non-alphanumeric characters.
+    return `${getStreamingUrl()}/videos/${fileName}.mp4`;
+}
+
+/**
  * Returns the url of an image file that represents the big thumbnail of a page with specified values. The only valid parameter for "fileName" is the first page name retrieved from getPageInfoUrl.
  */
 function getBigThumbUrl(galleryId, fileName) {
@@ -53,10 +73,12 @@ function getSmallThumbUrl(galleryId, fileName) {
 
 module.exports = {
     getBaseUrl,
+    getStreamingUrl,
     getTagsUrl,
     getPageInfoUrl,
     getGalleryUrl,
     getPageFileUrl,
+    getAnimeFileUrl,
     getBigThumbUrl,
     getSmallThumbUrl
 };
